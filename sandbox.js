3b9edccde3e441d76badf01cd75ef08cc3fbbf07
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-07-11 15:33:00 -0600

    Arrays and objects

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,4 @@
-var character = 'mario';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
-});
+var names = ["Luigi", "Mario", "Yoshi"];
+names.push("Toad");
+names.push(3);
+
---
+++ sandbox.ts
@@ -0,0 +1,10 @@
+let names: String[] = ["Luigi","Mario","Yoshi"];
+
+names.push("Toad");
+
+let ninja =
+{
+    name:"Mario",
+    belt: "Black",
+    age: 30
+}
